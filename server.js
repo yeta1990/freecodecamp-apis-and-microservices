f899c7bfc6e0df4c4b2853e91a66b91cb60bb01d
repl.it user <replituser@example.com>
2021-07-13 10:45:33 +0000

    third projectÂ

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
 var cors = require('cors');
+var bodyParser = require('body-parser');
+const dns = require('dns');
+const {nanoid} = require('nanoid');
 
 var app = express();
 
@@ -14,10 +17,16 @@ var port = process.env.PORT || 3000;
 /** this project needs a db !! **/ 
 // mongoose.connect(process.env.DB_URI);
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }); 
+
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+
+
+app.use(bodyParser.urlencoded({extended: 'false'}));
+app.use(bodyParser.json());
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -35,3 +44,103 @@ app.get("/api/hello", function (req, res) {
 app.listen(port, function () {
   console.log('Node.js listening ...');
 });
+
+
+const urlSchema = new mongoose.Schema({
+  original_url : {type: String, required: true},
+  short_url : {type: String, required: true}
+
+});
+const Urlmodel = mongoose.model('Urlmodel', urlSchema);
+
+
+app.post('/api/shorturl/new', (req,res) => {
+  //avergiuar último id almacenado en bbdd
+  //crear modelo a almacenar
+  // almacenarlo
+  var shortId = nanoid(5);
+  console.log(shortId);
+  
+  
+  var urltosave = new Urlmodel({"original_url": req.body.url, "short_url": nanoid(5)});
+
+  urltosave.save(function(err, doc) {
+    if(err) return console.error(err);
+
+     // data(null, data); 
+  });
+
+  res.json(urltosave);
+
+});
+
+
+app.get('/api/shorturl/:id', (req,res) => {
+
+  var urltosave = findUrlByShortid(req.params.id, (err2, done)=> {
+        if (err2) return console.error(err2);
+        console.log("enviado" + done);
+        res.redirect(301, done.original_url);
+        
+
+       // res.json(done.original_url);
+
+      });
+    
+  });
+
+
+
+  
+
+
+
+
+
+var findUrlByShortid = function(shorturlid, done) {
+
+  Urlmodel.findOne({short_url: shorturlid}, function(err,foundDoc){
+
+    if (err) {
+        return console.error(err);
+      } else {
+        console.log("localizado " + foundDoc);
+        done(null,foundDoc);
+    }
+
+  })
+};
+
+
+/*
+var lookForLastId = function(original_url,lastIdfound) {
+  
+  var id;
+
+
+  Urlmodel.find({}).sort({short_url: 'desc'}).limit(1).exec(function(err,lastId) {
+    if(err) {
+      console.log(err);
+    }else {
+      id = +foundId.short_url;
+      console.log('2.encontrado id. ' + id);
+      console.log('3.encontrado id ' + lastId);
+      
+      lastId(null, id);
+      
+    }
+
+  }
+  );
+  console.log("4.el id es " + id);
+  lastIdfound(id);
+ 
+}
+*/
+/*var createUrl
+
+
+
+dns.lookup('example.com', options, (err, address, family) =>
+  console.log('address: %j family: IPv%s', address, family));
+*/
