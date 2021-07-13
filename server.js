004a7ec00a92cc56888293f446a6324a806dc0a0
repl.it user <replituser@example.com>
2021-07-13 11:05:18 +0000

    fourth exercise

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+
+//mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/exercise-track' )
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }); 
 
 app.use(cors())
 
@@ -17,6 +19,130 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+
+
+const exerciseSchema = new mongoose.Schema({
+  description : {type: String, required: true},
+  duration : {type: Number, required: true},
+  date : String
+
+})
+
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+const userSchema = new mongoose.Schema({
+  username : {type: String, required: true},
+  log: [exerciseSchema]
+  
+});
+const User = mongoose.model('User', userSchema);
+
+
+
+
+app.post("/api/exercise/new-user", (req,res) => {
+
+  var usertosave = new User({"username": req.body.username});
+
+  usertosave.save(function(err, doc) {
+    if(err) return console.error(err);
+    res.json(doc);
+      
+  });
+  
+
+});
+
+app.get('/api/exercise/users', (req,res)=> {
+  User.find({}, function(err, users){
+    if (!err) {
+      res.json(users);
+    }
+
+  })
+
+});
+
+
+
+app.post("/api/exercise/add", (req,res) => {
+
+   var exercisetosave = new Exercise({
+  "description": req.body.description, 
+  "duration": parseInt(req.body.duration),
+  "date": req.body.date});
+
+  if(exercisetosave.date === ''){
+    exercisetosave.date = new Date().toISOString().substring(0,10);
+  }
+
+  User.findByIdAndUpdate(
+    req.body.userId, 
+    {$push: {log: exercisetosave}},
+    {new: true},
+    (error, updatedUser)=>{
+      let responseObject = {};
+      responseObject['_id'] = updatedUser.id;
+      responseObject['username'] = updatedUser.username;
+      responseObject['date'] = new Date(exercisetosave.date).toDateString();
+      responseObject['description'] = exercisetosave.description;
+      responseObject['duration'] = exercisetosave.duration;
+      res.json(responseObject);
+
+    }
+  );
+
+
+});
+
+app.get('/api/exercise/log', (request, response) => {
+  User.findById(request.query.userId, (error, result)=> {
+    if(!error){
+      let responseObject = result;
+
+
+      if(request.query.from || request.query.to) {
+
+          let fromDate = new Date(0);
+          let toDate = new Date();
+
+          if(request.query.from){
+            fromDate = new Date(request.query.from)
+
+          }
+
+          if(request.query.to){
+            toDate = new Date(request.query.to)
+          }
+
+          fromDate = fromDate.getTime();
+          toDate = toDate.getTime();
+
+          responseObject.log = responseObject.log.filter((session) => {
+            let sessionDate = new Date(session.date).getTime();
+
+            return sessionDate >= fromDate && sessionDate <= toDate
+
+          })
+
+      }
+
+      if(request.query.limit){
+        responseObject.log = responseObject.log.slice(0, request.query.limit);
+      }
+
+
+
+      responseObject['count'] = result.log.length;
+      response.json(responseObject);
+
+    }
+
+  })
+
+} );
+
+
 
 
 // Not found middleware
