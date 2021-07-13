3b3d72eb014f517ccf760d239ad13665d4978562
repl.it user <replituser@example.com>
2021-07-13 09:58:39 +0000

    first project

--- server.js
+++ server.js
@@ -30,3 +30,101 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+var isInteger = function(req,res){
+  if (Number.isInteger(+req)) {
+      console.log(req + ' número')
+   return true } else {
+     console.log('mierda');
+   }
+
+}
+
+app.get("/api/timestamp/",
+  function(req,res) {
+    res.json(
+        mensaje(new Date(), (err, message) => {
+          if (err) throw console.log(err);
+          done(null, message);
+        })
+      );
+
+  }
+);
+app.get('/api/timestamp/:date_string', 
+  
+  function(req,res,next) {
+    if (isInteger(req.params.date_string, function(err,data)
+    {
+      if(err) throw console.log(err);
+      done(null, true)
+
+      })
+    )
+    {
+        
+        
+       res.json(
+        mensaje(new Date(+req.params.date_string), (err, message) => {
+          if (err) throw console.log(err);
+          done(null, message);
+        })
+      );
+
+    } else if 
+    (
+      (req.params.date_string.length = 10)
+      &&
+      (req.params.date_string.match(/[0-9]{4}-[0-12]{2}-[0-9]{2}/g)) 
+    ){
+        res.json(
+        mensaje(new Date(req.params.date_string), (err, message) => {
+          if (err) throw console.log(err);
+          done(null, message);
+        })
+      );
+         
+    } else {
+      res.json({"error" : "Invalid Date" });
+    }
+
+   /*
+
+    if (req.params.date_string != null){
+      
+      res.json(
+        mensaje(new Date(req.params.date_string), (err, message) => {
+          if (err) throw console.log(err);
+          done(null, message);
+        })
+      );
+
+    }else {
+            res.json(
+        mensaje(new Date(), (err, message) => {
+          if (err) throw console.log(err);
+          done(null, message);
+        })
+      );
+
+    }
+*/
+
+   
+    
+    
+});
+
+var mensaje = function(dateInput){
+  
+  var messageOutput = 
+    {unix: dateInput.getTime(), utc: dateInput.toUTCString()};
+  return messageOutput;
+ 
+
+}
+
+var isValidDate = function(req,res) {
+  
+  return false;
+};
