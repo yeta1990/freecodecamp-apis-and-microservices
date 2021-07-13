38d1169f2dfa8dd67a6066e86e80d1a20a6ddc25
repl.it user <replituser@example.com>
2021-07-13 11:23:25 +0000

    fifth exercise

--- server.js
+++ server.js
@@ -2,12 +2,21 @@
 
 var express = require('express');
 var cors = require('cors');
+const bodyParser = require('body-parser')
 
 // require and use "multer"...
+var multer  = require('multer')
+var upload = multer()
+
+
+
 
 var app = express();
 
 app.use(cors());
+
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
@@ -20,4 +29,23 @@ app.get('/hello', function(req, res){
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
+});
+
+
+app.post('/api/fileanalyse', upload.single('upfile'),function(req,res,next){
+    const file = req.file;
+    const responseObject = {}
+    if(!file){
+      
+      const error = new Error('Please upload a file');
+      error.httpStatusCode = 400;
+      return next(error);
+    }
+    console.log(file.name);
+    responseObject['name'] = file.originalname;
+    responseObject['type'] = file.mimetype;
+    responseObject['size'] = file.size;
+    res.json(responseObject);
+  
+
 });
